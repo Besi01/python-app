6d18a12cc7d1a300543ba53655dbda08eaffbbc4
Besi <besimshima18@Gmail.com>
2025-11-17 12:12:14 +0100

    Adds ci github workflow

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
-        'hostname': 'here'
+        'hostname': 'here:1'
     })
 
 @app.route('/api/v1/healthz')
